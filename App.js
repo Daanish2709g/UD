13142609f1bd01baae3c3177ad22e281722dd215
Daanish ilahi <daanishsumkesula@gmail.com>
2024-11-25 17:23:46 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Footer from './Footer';
+import './App.css';
+
+function App() {
+    return (
+        <div className="app-container">
+            <div className="content">
+               
+            </div>
+            <Footer />
+        </div>
+    );
+}
+
+export default App;
